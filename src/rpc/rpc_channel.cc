41afcc5b37f6319160dd89fd5b7f7c2bc27bdb34
Chaobin Zhang <zhchbin@gmail.com>
2015-01-11 23:42:49 +0800

    RPC channel and internal message proto.

---
+++ src/rpc/rpc_channel.cc
@@ -0,0 +1,36 @@
+#include "rpc/rpc_channel.h"
+
+#include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
+#include "google/protobuf/descriptor.h"
+#include "google/protobuf/message.h"
+#include "proto/rpc_message.pb.h"
+
+namespace rpc {
+
+RpcChannel::RpcChannel() {
+
+}
+
+RpcChannel::~RpcChannel() {
+}
+
+void RpcChannel::CallMethod(const pb::MethodDescriptor* method,
+                            pb::RpcController* controller,
+                            const pb::Message* request,
+                            pb::Message* response,
+                            pb::Closure* done) {
+  scoped_ptr<RpcMessage> message(new RpcMessage());
+  message->set_id(1);  // FIXME: Generate ID.
+  message->set_type(RpcMessage::REQUEST);
+  message->set_service(method->service()->full_name());
+  message->set_method(method->name());
+  message->set_request(request->SerializeAsString());
+
+  // Connect to Rpc server.
+  // Send the serialized data out.
+  // Wait for the response.
+  // Call done->Run().
+}
+
+}  // namespace rpc
---
+++ src/rpc/rpc_channel.h
@@ -0,0 +1,45 @@
+#ifndef  RPC_RPC_CHANNEL_H_
+#define  RPC_RPC_CHANNEL_H_
+
+#include "google/protobuf/service.h"
+
+#include "base/macros.h"
+
+namespace google {
+namespace protobuf {
+
+class MethodDescriptor;
+class RpcController;
+class Messae;
+class Closure;
+
+}  // namespace protobuf
+}  // namespace google
+
+namespace pb = google::protobuf;
+
+namespace rpc {
+
+class RpcChannel : public google::protobuf::RpcChannel {
+ public:
+  RpcChannel();
+  virtual ~RpcChannel();
+
+  // Call the given method of the remote service.  The signature of this
+  // procedure looks the same as Service::CallMethod(), but the requirements
+  // are less strict in one important way:  the request and response objects
+  // need not be of any specific class as long as their descriptors are
+  // method->input_type() and method->output_type().
+  virtual void CallMethod(const pb::MethodDescriptor* method,
+                          pb::RpcController* controller,
+                          const pb::Message* request,
+                          pb::Message* response,
+                          pb::Closure* done) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(RpcChannel);
+};
+
+}  // namespace rpc
+
+#endif  // RPC_RPC_CHANNEL_H_
