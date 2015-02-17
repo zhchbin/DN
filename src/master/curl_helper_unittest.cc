088699e1c1afb4e64f2de1cf993ce572c448c7c1
Chaobin Zhang <zhchbin@gmail.com>
2015-02-17 13:16:44 +0800

    [TEST] Use license file instead of readme.
    Change of LICENSE is less than README.md.

--- src/master/curl_helper_unittest.cc
+++ src/master/curl_helper_unittest.cc
@@ -14,16 +14,16 @@ TEST(CurlHelperTest, GetWithMD5) {
   base::ScopedTempDir temp_dir;
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
 
-  base::FilePath readme_file_path;
-  PathService::Get(base::DIR_SOURCE_ROOT, &readme_file_path);
-  readme_file_path = readme_file_path.AppendASCII("README.md");
+  base::FilePath license_file_path;
+  PathService::Get(base::DIR_SOURCE_ROOT, &license_file_path);
+  license_file_path = license_file_path.AppendASCII("LICENSE");
 
   base::FilePath file_path = temp_dir.path().AppendASCII("tmp_file");
   CurlHelper curl_helper;
-  const std::string kReadmeURL =
-      "https://raw.githubusercontent.com/zhchbin/DN/master/README.md";
-  EXPECT_EQ(curl_helper.Get(kReadmeURL, file_path),
-            common::GetMd5Digest(readme_file_path));
+  const std::string klicenseURL =
+      "https://raw.githubusercontent.com/zhchbin/DN/master/LICENSE";
+  EXPECT_EQ(curl_helper.Get(klicenseURL, file_path),
+            common::GetMd5Digest(license_file_path));
 }
 
 }  // namespace master
